34ce1a7e8dba406aabe547dca09653a23d2658d7
nishit <nishitmohanan@gmail.com>
2019-04-07 02:09:33 +0530

    Added api endpoint urls

--- py_bin/core/urls.py
+++ py_bin/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from rest_framework import routers
+
 from core import views
 
-urlpatterns = [
-    path('test/', views.TestView.as_view(), name='test'),
-]
+router = routers.DefaultRouter()
+router.register("api/bin", views.BinViewSet, "bin")
+
+urlpatterns = router.urls
