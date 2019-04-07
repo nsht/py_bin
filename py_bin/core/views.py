ea34ceed2582b1f4cee89a3e5513a3ae9a668a4c
nishit <nishitmohanan@gmail.com>
2019-04-08 01:56:31 +0530

    Added custom create and list functions

--- py_bin/core/views.py
+++ py_bin/core/views.py
@@ -1,3 +1,7 @@
+import string
+import random
+import time
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,7 +10,28 @@ from rest_framework import viewsets, permissions
 from .models import Bin
 from .serializers import *
 
+
 class BinViewSet(viewsets.ModelViewSet):
+    # TODO only show public objects
     queryset = Bin.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = BinSerializer
+
+    def create(self, request):
+        # TODO check for slug collisions
+        slug = self.generate_slug()
+        bin = Bin(
+            slug=slug,
+            content=request.data.get("content"),
+            content_format=request.data.get("content_format"),
+        )
+        bin.save()
+        import pdb
+        pdb.set_trace()
+        return Response(status=status.HTTP_200_OK)
+
+# TODO Move function to helper file
+    def generate_slug(self):
+        random.seed(time.time())
+        chars = string.ascii_letters + string.digits
+        return "".join(random.choice(chars) for _ in range(7))
