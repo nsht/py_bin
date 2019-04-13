1fcf0e8c12df0946d8e2817355dd3d979627fdce
nishit <nishitmohanan@gmail.com>
2019-04-14 01:13:14 +0530

    Moved create logic to serializers

--- py_bin/core/serializers.py
+++ py_bin/core/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+
 from .models import Bin
+from .utils import generate_slug
+
 
 class BinSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bin
-        fields = '__all__'
+        fields = "__all__"
+
+    def create(self, validated_data):
+        slug = generate_slug()
+        chk_slug = Bin.objects.filter(slug=slug).first()
+        # keep generating new slugs until no collitions are detected
+        # very low probability of happening twice.
+        while chk_slug is not None:
+            slug = generate_slug()
+            chk_slug = Bin.objects.filter(slug=slug).first()
+        bin = Bin(
+            slug=slug,
+            content=validated_data.get("content"),
+            content_format=validated_data.get("content_format"),
+        )
+        if validated_data.get("password") != "" or validated_data is not None:
+            bin.protected = True
+            bin.is_public = False
+            # uses PBKDF2 by default, use check_password to verify passwords
+            bin.password = make_password(validated_data.get("password"))
+        bin.save()
+        return bin
--- py_bin/core/views.py
+++ py_bin/core/views.py
@@ -2,7 +2,6 @@ import string
 import random
 import time
 
-from django.contrib.auth.hashers import make_password
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,26 +18,17 @@ class BinViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = BinSerializer
 
-    def create(self, request):
-        slug = generate_slug()
-        chk_slug = Bin.objects.filter(slug=slug).first()
-        # keep generating new slugs until no collitions are detected
-        # very low probability of happening twice.
-        while chk_slug is not None:
-            slug = generate_slug()
-            chk_slug = Bin.objects.filter(slug=slug).first()
-        bin = Bin(
-            slug=slug,
-            content=request.data.get("content"),
-            content_format=request.data.get("content_format"),
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
-        if request.data.get("password") != "":
-            bin.protected = True
-            bin.is_public = False
-            # uses PBKDF2 by default, use check_password to verify passwords
-            bin.password = make_password(request.data.get("password"))
-        bin.save()
-        return Response(status=status.HTTP_200_OK)
+
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 # TODO
